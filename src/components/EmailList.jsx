37c8fed983a67aecc6374c11b7603ab99da2cec7
adheelali <adheelali2@gmail.com>
2022-11-25 01:08:01 -0800

    style error

--- src/components/EmailList.jsx
+++ src/components/EmailList.jsx
@@ -1,8 +1,11 @@
-import { ArrowDropDown, MoreVert, Redo } from '@mui/icons-material'
+import { ArrowDropDown, ChevronLeft, ChevronRight, MoreVert, Redo } from '@mui/icons-material'
 import { Checkbox, IconButton } from '@mui/material'
 import React from 'react'
 import './emailList.css'
-
+import Section from './UI/Section'
+import Inbox from '@mui/icons-material/Inbox'
+import { People } from '@mui/icons-material'
+import { LocalOffer } from '@mui/icons-material'
 
 function EmailList() {
   return (
@@ -20,6 +23,19 @@ function EmailList() {
             <MoreVert/>
           </IconButton>
         </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        <Section Icon={Inbox} title="Primary" color="red" selected />
+        <Section Icon={People} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOffer} title="Promotions" color="green" />
       </div>
     </div>
   )
